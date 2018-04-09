6bdeae918a141e4f6808fde4b9634a2c0200d4c1
Robin <robin@chilio.net>
2018-04-09 11:47:23 +0200

    add missing include

---
+++ chord_internal.h
@@ -0,0 +1,8 @@
+#ifndef _LIBCHORD_INT_H
+#define _LIBCHORD_INT_H
+#include "chord.h"
+struct node mynode;
+struct node predecessor;
+struct fingertable_entry fingertable[FINGERTABLE_SIZE];
+struct node successorlist[FINGERTABLE_SIZE];
+#endif
